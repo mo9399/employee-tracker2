e801188d16367a14a77b496cabca169d8b86a797
your name <you@example.com>
2022-07-08 15:40:18 -0700

    add viewEmployess function to server.js

--- server.js
+++ server.js
@@ -95,3 +95,17 @@ const viewRoles = () => {
       }
     );
   };
+
+// View employees
+const viewEmployees = () => {
+    db.query(
+      `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;`,
+      (err, res) => {
+        if (err) throw err;
+        console.table(res);
+  
+        userOptions();
+      }
+    );
+  };
+  
