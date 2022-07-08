e07f50810b86e1aee82f9a8359cb50a9fd2c240b
your name <you@example.com>
2022-07-08 15:28:02 -0700

    add employee seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -20,3 +20,16 @@ VALUES
 ('I.T. Assistant', 95000, 4),
 ('Security Manager', 65000, 5),
 ('Loss Provention', 45000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Micheal', 'Jordan', 1, NULL),
+  ('Kobe', 'Bryant', 2, 1),
+  ('Scottie', 'Pippen', 3, NULL),
+  ('Lebron', 'James', 4, 3),
+  ('Damian', 'Lillard', 5, NULL),
+  ('Magic', 'Johnson', 6, 5),
+  ('Tracy', 'McGrady', 7, NULL),
+  ('Allen', 'Iverson', 8, 7),
+  ('Dwayne', 'Wade', 9, NULL),
+  ('Larry', 'Bird', 10, 9); 
