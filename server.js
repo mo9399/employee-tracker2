50ba581df9d1bf9cdb787a396b2e30db58e4cb2b
Mo <mohamed.abdullahi@yahoo.com>
2022-07-08 21:01:17 -0700

    fix console.table error by removing line 4 from server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const inquirer = require("inquirer");
 const db = require("./db/connection.js");
-const console = require("console.table");
+
 
 
 // Connect to the database
